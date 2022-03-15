4055311bf3c45c5dd1810c3402f89b31d4fae051
David FYON <davidfyon@gmail.com>
2022-03-15 14:44:34 +0100

    change favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -12,7 +12,7 @@ export default {
       { name: "format-detection", content: "telephone=no" },
     ],
     link: [
-      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+      { rel: "icon", type="image/svg+xml", href: "/favicon.svg" },
       {
         rel: "stylesheet",
         href: "https://fonts.googleapis.com/css2?family=Tinos&family=Inter:wght@400;500;600;700;800;900&display=fallback",
