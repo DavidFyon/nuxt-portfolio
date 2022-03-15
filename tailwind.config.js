6063076da6cf2c1ba5ea1438869ae6d48b6ae555
David FYON <davidfyon@gmail.com>
2022-03-15 20:28:15 +0100

    fix skill bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  purge: ["./index.html", './public/index.html', "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
@@ -32,20 +32,6 @@ module.exports = {
       },
       opacity: {
         90: "0.90",
-      },
-      width: {
-        "1/12": "8.333333%",
-        "2/12": "16.666667%",
-        "3/12": "25%",
-        "4/12": "33.333333%",
-        "5/12": "41.666667%",
-        "6/12": "50%",
-        "7/12": "58.333333%",
-        "8/12": "66.666667%",
-        "9/12": "75%",
-        "10/12": "83.333333%",
-        "11/12": "91.666667%",
-        "12/12": "100%",
       },
     },
     container: {
